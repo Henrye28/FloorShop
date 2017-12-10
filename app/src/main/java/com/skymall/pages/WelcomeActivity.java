59a363337a10e9126d1f778e7a5dea2e9e2d8797
Henrye28 <hongliye28@gmail.com>
2017-12-11 00:25:55 +0800

    picking up welcom page

--- app/src/main/java/com/skymall/pages/WelcomeActivity.java
+++ app/src/main/java/com/skymall/pages/WelcomeActivity.java
@@ -4,11 +4,9 @@ package com.skymall.pages;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.dd.processbutton.FlatButton;
 import com.skymall.R;
 
 import cn.bmob.v3.BmobUser;
@@ -16,8 +14,6 @@ import cn.bmob.v3.BmobUser;
 
 public class WelcomeActivity  extends AppCompatActivity {
 
-    private FlatButton signIn;
-    private FlatButton signUp;
     Intent in;
 
 
@@ -37,27 +33,6 @@ public class WelcomeActivity  extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.welcome_page);
-
-        signIn = (FlatButton)findViewById(R.id.signIn);
-        signUp = (FlatButton)findViewById(R.id.signUp);
-
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                in = new Intent(WelcomeActivity.this, LoginWithMobilePage.class);
-
-                startActivity(in);
-            }
-        });
-
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                in = new Intent(WelcomeActivity.this, RegisterMobilePage.class);
-                startActivity(in);
-            }
-        });
-
     }
 }
 
